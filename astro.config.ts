fbb4b0a2e002c362498307629110787561167b94
Giusy <gc@datespotz.com>
2024-05-21 21:20:08 -0400

    Initial commit

---
+++ astro.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import UnoCSS from 'unocss/astro';
+
+export default defineConfig({
+  // used to generate images
+  site: "https://bye-wanxiety-ncw5p0mas-giusy-castigliones-projects.vercel.app",
+  trailingSlash: 'ignore',
+  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  vite: {
+    optimizeDeps: {
+      exclude: ['@resvg/resvg-js'],
+    },
+  },
+});
