b96cd234967724057fb52beeeb4917f602ca1289
giusyc <g@wanxiety.com>
2024-07-10 01:00:26 -0400

    analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   // used to generate images
+  output: 'hybrid',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   site: "https://bye-wanxiety-ncw5p0mas-giusy-castigliones-projects.vercel.app",
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
