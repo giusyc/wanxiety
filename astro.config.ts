c006aed96e26ab9e3634f5e4fbcf60df382506e3
giusyc <g@wanxiety.com>
2024-07-10 00:55:33 -0400

    remove analytics, breaks code

--- astro.config.ts
+++ astro.config.ts
@@ -1,25 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
-import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   // used to generate images
   site: "https://bye-wanxiety-ncw5p0mas-giusy-castigliones-projects.vercel.app",
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({
-    injectReset: true
-  })],
+  integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js']
-    }
-  },
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
+      exclude: ['@resvg/resvg-js'],
     },
-  })
+  },
 });
