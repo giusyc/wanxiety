2cf2540a927869d02a0d0635c21e07350dbf416a
giusyc <g@wanxiety.com>
2024-07-10 00:51:42 -0400

    analytics and ui fix

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   // used to generate images
   site: "https://bye-wanxiety-ncw5p0mas-giusy-castigliones-projects.vercel.app",
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [sitemap(), UnoCSS({
+    injectReset: true
+  })],
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
-    },
+      exclude: ['@resvg/resvg-js']
+    }
   },
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
